2095dc541c553a299767125988a77615d76f44f8
skyhanullee <leeskyha@gmail.com>
2023-01-26 05:21:48 -0800

    formatted the date to show month and day only

--- blog-homepage/src/ArticleDetails.jsx
+++ blog-homepage/src/ArticleDetails.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
 function ArticleDetails({ authorImage, authorName, postedDate, minutesToRead }) {
+  function formattedDate(date) {
+    return new Date(date).toLocaleDateString('en-Us', 
+      {month: 'short', day: 'numeric'
+    });
+  };
+
   return (
     <div className='article-details'>
-      <img src={authorImage} alt={authorName}/>
-      <h4>{authorName}</h4>
-      <p>{postedDate}</p>
-      <p>{minutesToRead} min read</p>
+      <div className='author-image-container'>
+        <img className='author-image' src={authorImage} alt={authorName}/>
+      </div>
+      <div className='addtional-info-container'>
+        <h4>{authorName}</h4>
+        <div>
+          <p>{formattedDate(postedDate)}</p>
+          <p> | </p>
+          <p>{minutesToRead} min read</p>
+        </div>
+      </div>
     </div>
   )
 }
