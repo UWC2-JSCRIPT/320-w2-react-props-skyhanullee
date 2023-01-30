b5e2e97a07268c5ceff45f0d8467cc81e2364009
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-01-30 01:01:25 -0800

    uncommented the <a> tag and prop type for link

--- blog-homepage/src/ArticleImage.jsx
+++ blog-homepage/src/ArticleImage.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 function ArticleImage({ image, altDescription, link }) {
   return (
-    // <a href={link} className='article-image-container'>
+    <a href={link} className='article-image-container'>
       <img className='article-image' src={image} alt={altDescription}/>
-    // </a>
+    </a>
   )
 }
 
 ArticleImage.propTypes = {
   image: PropTypes.string.isRequired,
   altDescription: PropTypes.string.isRequired,
-  // link: PropTypes.string.isRequired
+  link: PropTypes.string.isRequired
 };
 
 export default ArticleImage
