e6977ec4543c38233a3df69d7b7baec4e46e295f
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-01-30 00:18:00 -0800

    added feature component and prop types

--- blog-homepage/src/ArticleBody.jsx
+++ blog-homepage/src/ArticleBody.jsx
@@ -1,38 +1,53 @@
 import React from 'react';
 import ArticleDetails from './ArticleDetails';
 import ArticleSummary from './ArticleSummary';
+import ArticleFeature from './ArticleFeature';
+import PropTypes from 'prop-types';
 
-function ArticleBody({ title, description, author, postedDate, minutesToRead }) {
-  const { name, image } = author;
+// function ArticleBody({ title, description, author, postedDate, minutesToRead, hasAudioAvailable, memberPreview }) {
+function ArticleBody({ article }) {
+  const { title, description, link, author, postedDate, minutesToRead, hasAudioAvailable, memberPreview } = article;
+
+  // const { name, image, isMediumMember } = author;
+
   return (
     <div className='article-body'>
-      <ArticleSummary
-        title={title}
-        description={description}
-      />
-
-      <ArticleDetails
-        authorImage={image}
-        authorName={name}
-        postedDate={postedDate}
-        minutesToRead={minutesToRead}
-
-      />
+      <div className='article-card-top-container'>
+        <ArticleFeature
+            featureName={'audio'}
+            hasFeatureAvailable={hasAudioAvailable}
+          />
+          <ArticleFeature
+            featureName={'member'}
+            hasFeatureAvailable={memberPreview}
+          />
+          <ArticleSummary
+            title={title}
+            description={description}
+            link={link}
+          />
+      </div>
+      <div className='article-card-bottom-container'>
+        <ArticleDetails
+          authorImage={author.image}
+          authorName={author.name}
+          postedDate={postedDate}
+          minutesToRead={minutesToRead}
+          isMediumMember={author.isMediumMember}
+        />
+      </div>
     </div>
   )
 }
 
-// function ArticleBody({ title, description, author, date, minutesToRead }) {
-//   return (
-//     <div>
-//       <h1>{title}</h1>
-//       <h4>{description}</h4>
-//       {/* <img src={author.image} alt={author.name}/> */}
-//       {/* <h4>{author.name}</h4> */}
-//       <h5>{date}</h5>
-//       <h5>{minutesToRead}</h5>
-//     </div>
-//   )
-// }
+ArticleBody.propType = {
+  title: PropTypes.string.isRequired, 
+  description: PropTypes.string.isRequired, 
+  author: PropTypes.string.isRequired, 
+  postedDate: PropTypes.string.isRequired, 
+  minutesToRead: PropTypes.number.isRequired, 
+  hasAudioAvailable: PropTypes.bool.isRequired, 
+  memberPreview: PropTypes.bool.isRequired
+};
 
 export default ArticleBody
