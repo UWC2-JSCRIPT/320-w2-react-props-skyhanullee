fb13d466c2d7b05bb1023e2aefc8d87420388013
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-01-30 00:20:52 -0800

    created component to render audio preview and member preview notices

---
+++ blog-homepage/src/ArticleFeature.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ArticleFeature({ featureName, hasFeatureAvailable }) {
+  if (hasFeatureAvailable === true) {
+    if (featureName === 'audio') {
+      return <p className='audio-available'> Audio Available</p>
+    }
+    if (featureName === 'member') {
+      return <p className='member-preview'>Member Preview</p>
+    }
+  }
+  return null;
+}
+
+ArticleFeature.propTypes = {
+  featureName: PropTypes.string.isRequired, 
+  hasFeatureAvailable: PropTypes.bool.isRequired
+};
+
+export default ArticleFeature
