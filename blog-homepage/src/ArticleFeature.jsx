0d18cf03d9ddf61ae61ee248c99a4873399e8bc7
skyhanullee <leeskyha@gmail.com>
2023-01-31 15:50:33 -0800

    changed text to stick out more

--- blog-homepage/src/ArticleFeature.jsx
+++ blog-homepage/src/ArticleFeature.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 function ArticleFeature({ featureName, hasFeatureAvailable }) {
   if (hasFeatureAvailable === true) {
     if (featureName === 'audio') {
-      return <p className='audio-available'> Audio Available</p>
+      return <p className='audio-available'>* Audio Available *</p>
     }
     if (featureName === 'member') {
-      return <p className='member-preview'>Member Preview</p>
+      return <p className='member-preview'>* Member Preview *</p>
     }
   }
   return null;
