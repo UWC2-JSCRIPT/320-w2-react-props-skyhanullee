041df66cb99214bbefb6baed11b1330f5c6adcf2
skyhanullee <leeskyha@gmail.com>
2023-01-31 15:48:53 -0800

    added useState for bookmark color change

--- blog-homepage/src/ArticleBookmark.jsx
+++ blog-homepage/src/ArticleBookmark.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons'
@@ -6,10 +7,16 @@ import { faBookmark as faBookmarkSolid } from '@fortawesome/free-solid-svg-icons
 
 function ArticleBookmark() {
   library.add(faBookmarkRegular, faBookmarkSolid);
+
+  const [isBookmarked, setIsBookmarked] = useState(true);
+
   return (
-    <div className='article-bookmark'>
-      <FontAwesomeIcon icon="fa-regular fa-bookmark" />
-      {/* <FontAwesomeIcon icon="fa-solid fa-bookmark" /> */}
+    <div className='article-bookmark' onClick={() => {setIsBookmarked(!isBookmarked)}}>
+      {
+        isBookmarked ? 
+        <FontAwesomeIcon icon='fa-regular fa-bookmark'/> : 
+        <FontAwesomeIcon icon='fa-solid fa-bookmark' color='green' />
+      }
     </div>
 
   )
